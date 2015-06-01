28e7ab1ddfd1d68ccfc85087a2e8193163a56a1e
Bobby Brennan <bobby.brennan@gmail.com>
2015-06-01 12:50:30 -0400

    Add welcome dialog

--- src/main/groovy/com/smartbear/lucybot/PortalAction.java
+++ src/main/groovy/com/smartbear/lucybot/PortalAction.java
@@ -23,18 +23,52 @@ import com.wordnik.swagger.models.Swagger;
 import java.awt.Desktop;
 import java.net.URI;
 
+/**
+ *
+ * @author Bobby Brennan
+ */
+
 @ActionConfiguration(actionGroup = "EnabledWsdlProjectActions")
 public class PortalAction extends AbstractSoapUIAction<WsdlProject> {
-		private static final String VERSION = Form.class.getName() + Form.VERSION;
-
-	  private XFormDialog dialog;
+		private static final String VERSION = ExportForm.class.getName() + ExportForm.VERSION;
+		private static final String[] WELCOME_OPTIONS = {
+			"See an example",
+			"Try an interactive tutorial",
+			"Upload my API"
+		};
+		private XFormDialog exportDialog;
+		private XFormDialog welcomeDialog;
 
     public PortalAction() {
         super("PortalAction", "Create LucyBot Portal", "Creates a LucyBot portal");
+				welcomeDialog = ADialogBuilder.buildDialog(WelcomeForm.class);
+				XFormRadioGroup actions = (XFormRadioGroup) welcomeDialog.getFormField(WelcomeForm.ACTION);
+				actions.setOptions(WELCOME_OPTIONS);
     }
 
     @Override
     public void perform(WsdlProject project, Object param) {
+			while (welcomeDialog.show()) {
+				String choice = welcomeDialog.getValue(WelcomeForm.ACTION);
+				if (choice.equals(WELCOME_OPTIONS[0])) {
+					showExample();
+				} else if (choice.equals(WELCOME_OPTIONS[1])) {
+					showTutorial();
+				} else {
+					uploadAPI(project);
+				}
+			}
+		}
+
+		private void showExample() {
+			openURL("https://lucybot.com/portals/hacker_news");
+		}
+
+		private void showTutorial() {
+      openURL("https://lucybot.com/demo");
+		}
+
+		private void uploadAPI(WsdlProject project) {
 			if (project.getInterfaces(RestServiceFactory.REST_TYPE).isEmpty()) {
 					UISupport.showErrorMessage("Project is missing REST APIs");
 					return;
@@ -42,26 +76,26 @@ public class PortalAction extends AbstractSoapUIAction<WsdlProject> {
 
 			// initialize form
 			XmlBeansSettingsImpl settings = project.getSettings();
-			if (dialog == null) {
-					dialog = ADialogBuilder.buildDialog(Form.class);
+			if (exportDialog == null) {
+					exportDialog = ADialogBuilder.buildDialog(ExportForm.class);
 
-					dialog.setValue(Form.VERSION, settings.getString(VERSION, "1.0"));
-					dialog.setValue(Form.HOST, "api.example.com");
+					exportDialog.setValue(ExportForm.VERSION, settings.getString(VERSION, "1.0"));
+					exportDialog.setValue(ExportForm.HOST, "api.example.com");
 
-					XFormRadioGroup apis = (XFormRadioGroup) dialog.getFormField(Form.API);
+					XFormRadioGroup apis = (XFormRadioGroup) exportDialog.getFormField(ExportForm.API);
 					String[] apiNames = ModelSupport.getNames(project.getInterfaces(RestServiceFactory.REST_TYPE));
 					apis.setOptions(apiNames);
 					apis.setValue(apiNames[0]);
 
-					XFormOptionsField protocols = (XFormOptionsField) dialog.getFormField(Form.PROTOCOLS);
+					XFormOptionsField protocols = (XFormOptionsField) exportDialog.getFormField(ExportForm.PROTOCOLS);
 					String[] protocolOptions = {"http", "https"};
 					protocols.setOptions(protocolOptions);
 					protocols.setSelectedOptions(protocolOptions);
 			}
 
-			while (dialog.show()) {
+			while (exportDialog.show()) {
 					try {
-							Object[] protocolObjs = ((XFormOptionsField) dialog.getFormField(Form.PROTOCOLS)).getSelectedOptions();
+							Object[] protocolObjs = ((XFormOptionsField) exportDialog.getFormField(ExportForm.PROTOCOLS)).getSelectedOptions();
 							if (protocolObjs.length == 0) {
 									throw new Exception("You must select at least one protocol");
 							}
@@ -71,15 +105,15 @@ public class PortalAction extends AbstractSoapUIAction<WsdlProject> {
 								selectedProtocols[i] = String.valueOf(protocolObjs[i]);
 							}
 
-							String api = dialog.getValue(Form.API);
+							String api = exportDialog.getValue(ExportForm.API);
               RestService service = (RestService) project.getInterfaceByName(api);
 							if (service.getEndpoints().length == 0) {
 									throw new Exception("Selected API must contain at least one endpoint");
 							}
 
-							String host = dialog.getValue(Form.HOST);
+							String host = exportDialog.getValue(ExportForm.HOST);
 
-							String version = dialog.getValue(Form.VERSION);
+							String version = exportDialog.getValue(ExportForm.VERSION);
 							if (StringUtils.isNullOrEmpty(version)) {
 									version = "1.0";
 							}
@@ -87,12 +121,12 @@ public class PortalAction extends AbstractSoapUIAction<WsdlProject> {
 							Swagger2Exporter exporter = new Swagger2Exporter(project);
 							String id = exporter.exportToLucyBot(service, version, host, selectedProtocols);
 			        String message = "Your LucyBot portal is ready! You can see it at ";
-							String url = "lucybot.com/trial/" + id;
+							String url = "lucybot.com/demo/" + id;
 							message += url + '\n';
 							message += "Do you want to go there now?";
 							boolean shouldOpen = UISupport.confirm(message, "Open LucyBot Portal");
 							if (shouldOpen) {
-              	Desktop.getDesktop().browse(new URI("https://" + url));
+              	openURL("https://" + url + "?nohelp=true");
 							}
 					} catch (Exception ex) {
 							UISupport.showErrorMessage(ex);
@@ -100,9 +134,27 @@ public class PortalAction extends AbstractSoapUIAction<WsdlProject> {
 			}
     }
 
+		private void openURL(String url) {
+			try {
+				URI uri = new URI(url);
+				Desktop.getDesktop().browse(uri);
+			} catch (Exception ex) {
+				UISupport.showErrorMessage(ex);
+			}
+		}
+
+		@AForm(name = "Welcome to LucyBot!",
+		       description = "LucyBot is a web-based tool for documenting and visualizing REST APIs. You can see demos and examples at lucybot.com")
+    public interface WelcomeForm {
+			@AField(name = "Action",
+			        description = "What would you like to do?",
+							type = AFieldType.RADIOGROUP)
+			public final static String ACTION = "Action";
+		}
+
 		@AForm(name = "Generate LucyBot Portal",
 		       description = "Create a LucyBot portal for one of your REST APIs. The portal will be private until you publish it.")
-		public interface Form {
+		public interface ExportForm {
 				@AField(name = "API",
 								description = "Select which REST API to view on LucyBot",
 								type = AFieldType.RADIOGROUP)
